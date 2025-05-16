4492457e9e5a8088e9bad453a39e20bb92e75567
Vladimir Shemetenko <ov.shtk@gmail.com>
2025-05-16 17:44:35 +0300

    BookService создан и подключён к Еврике.

---
+++ src/main/java/com/example/bookservice/model/Book.java
@@ -0,0 +1,22 @@
+package com.example.bookservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "books")
+public class Book {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String imageLink;
+}
